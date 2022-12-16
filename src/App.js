23d4e689c7a169eb4c519f2670c46fff20c50b38
ssantiago90 <salkin.santiago@gmail.com>
2022-12-15 21:08:39 -0300

    clase 3 jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./navbar.css";
 
+// Componente APP
 function App() {
+  const greeting = "Bienvenidos a mi Ecommerce!";
+
+  const mostrarCurso = () => "Comisión 43580 - React JS";
+
+  /*   function mostrarCurso(){
+    return "Comisión 43580 - React JS"
+  } */
+
+  function getRandomColor() {
+    return "darkgreen";
+  }
+
+  let styleHeader = {
+    backgroundColor: getRandomColor(),
+    margin: "20px 10px",
+    border: "solid 3px red",
+  };
+
+  // camelCase -> deEstaManera siempreConLaPrimerLetraEnMayusc
   return (
-    <section className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bienvenidos a React!!!!!
-        </a>
-      </div>
-    </section>
+    <>
+      <nav>Hola</nav>
+      <section className="App">
+        <div style={styleHeader} className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p style={{ color: "pink" }}>{mostrarCurso()}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {greeting}
+            <br />
+            {"Coderhouse."}
+            <br />
+            {10 + 20}
+          </a>
+          {/* soy un comentario */}
+        </div>
+      </section>
+    </>
   );
 }
 
